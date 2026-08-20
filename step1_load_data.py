09f8bc4d9acadc6f248377dd283a088c9950763e
dsiddhi20 <166084767+dsiddhi20@users.noreply.github.com>
2026-08-20 12:15:21 +0530

    Update step1_load_data.py

--- step1_load_data.py
+++ step1_load_data.py
@@ -3,8 +3,8 @@ import pandas as pd
 train_df = pd.read_csv("data/train.csv")
 test_df = pd.read_csv("data/test.csv")
 
-print("✅ Train data shape:", train_df.shape)
-print("✅ Test data shape:", test_df.shape)
+print("Train data shape:", train_df.shape)
+print("Test data shape:", test_df.shape)
 
 print("\nTrain columns:")
 print(train_df.columns)
